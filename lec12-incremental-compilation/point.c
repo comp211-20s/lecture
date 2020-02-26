2030797e8072fb8cae2012943724862eba2d95ab
Kris Jordan <kris@cs.unc.edu>
2020-02-26 11:09:23 +0000

    Add scaffolding for lec12

---
+++ lec12-incremental-compilation/point.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Point {
+    double x;
+    double y;
+};
+
+// Forward Function Declarations
+// void Point_init(Point *this, double x, double y);
+// Point Point_add(const Point *this, const Point *to);
+void Point_print(const struct Point *this);
+
+int main()
+{
+    // Declare a struct Point variable
+    // Print its values
+}
+
+void Point_print(const struct Point *this)
+{
+    printf("(%f, %f)\n", (*this).x, (*this).y);
+}
